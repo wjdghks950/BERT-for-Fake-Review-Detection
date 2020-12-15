3d32349e335a31c94026a52e7a0bba888bda7e11
Jeonghwan Kim <jeonghwankim123@gmail.com>
2020-12-15 11:36:39 +0000

    Commiting the shell script for training execution

---
+++ train_amazon.sh
@@ -0,0 +1,1 @@
+python main.py --task amazon --logging_steps 100 --train_file amazon_train.csv --dev_file amazon_dev.csv $1 $2
---
+++ train_opspam.sh
@@ -0,0 +1,1 @@
+python main.py --task opspam --do_train --logging_steps 100 --train_file opspam_train.csv --test_file opspam_dev.csv $1 $2
